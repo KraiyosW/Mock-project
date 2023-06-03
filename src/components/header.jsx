53008251d0c9c2edf6c2c58082c557bff3858fce
KraiyosW <kraiyos.wanna@gmail.com>
2023-06-03 18:54:20 +0700

    feat: add header section

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return(
+        <div className=' h-[5rem] flex flex-col flex-warp items-start px-[50px] min-[768px]:px-[120px] bg-[#FF2950] font-[Anonymous Pro] text-[2rem]'>
+            <p className='font-[Anonymous Pro] text-[2rem] font-[700] text-white tracking-[.14rem] mt-[0.7rem]'>My Profile</p>
+        </div>
+    )
+
+}
+
+
+export default Header
