929fe6dd840f867c52670c90a95c2dd269ca53f0
KraiyosW <kraiyos.wanna@gmail.com>
2023-06-06 09:21:22 +0700

    feat: add my information sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import Header from './components/header'
+import Header from './components/header';
+import Information from './components/personal-information';
 import './App.css'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className='w-[100vw] h-[100vh] bg-[#F5ebee]'>
     <Header/>  
+    <Information/>
     </div>
   )
 }
---
+++ src/components/personal-information.jsx
@@ -0,0 +1,15 @@
+
+const Information = () => {
+
+    return (
+        <div className="flex flex-col flex-warp items-start mt-[3rem] ml-[10rem] px-[50px] min-[768px]:px-[120px] bg-[#F5ebee] ">
+            <p className="text-[2rem]" id='font1'> HI! </p>
+
+        </div>
+    );
+
+
+}
+
+
+export default Information
